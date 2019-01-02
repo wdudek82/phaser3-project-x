cfce5d7a13be441f4af731bc82a12b1ec84f6b48
Wojciech Dudek <wdudek82@gmail.com>
2019-01-02 13:57:19 +0100

    Additional parameters added to devServer webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    main: './src/index.js',
+    main: path.resolve(__dirname, 'src/index.js'),
   },
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: '.',
+    contentBase: path.join(__dirname, '.'),
+    hot: true,
+    compress: true,
+    port: 9000,
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Project X - Phaser 3',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
@@ -29,6 +34,7 @@ module.exports = {
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
       { test: /\.(csv|tsv)$/, use: ['csv-loader'] },
       { test: /\.xml$/, use: ['xml-loader'] },
+      { test: /\.js$/, use: ['source-map-loader'], enforce: 'pre' },
     ],
   },
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,15 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+// // Phaser webpack config
+// const phaserModule = path.join(__dirname, '/node_modules/phaser/');
+// const phaser = path.join(phaserModule, 'src/phaser.js');
+
+// --- Not finished ---
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
-    main: './src/index.js',
+    main: path.resolve(__dirname, 'src/index.js'),
+    // vendor: ['phaser'],
+  },
+  devServer: {
+    contentBase: '.',
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Project X - Phaser 3',
+      // chunks: ['vendor', 'main'],
     }),
   ],
   output: {
@@ -25,6 +35,7 @@ module.exports = {
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
       { test: /\.(csv|tsv)$/, use: ['csv-loader'] },
       { test: /\.xml$/, use: ['xml-loader'] },
+      // { test: /\.js$/, use: ['source-map-loader'], enforce: 'pre' },
     ],
   },
 };
